0e925c566992506ecae1a4d0abedecd7454c6e7a
Turhan <turhan.huseynov@gmail.com>
2019-12-14 10:34:06 +0300

    first draft created: routing implemented, inline css will be removed, image links will be replaced with given broken links, animations will be added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MovieList from './MovieList';
+import Movie from './Movie'
 
-function App() {
+var App = () => {
   return (
-    <div>
-      Hello World!
-    </div>
+    <Router>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={Movie} />
+    </Router>
   );
 }
 
