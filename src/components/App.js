44e87dfeb9fe2a45af95972a6dddc2623bbe165a
Turhan <turhan.huseynov@gmail.com>
2019-12-15 09:55:48 +0300

    minor improvements on css

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MovieList from './MovieList';
 import Movie from './Movie'
 
 var App = () => {
   return (
     <Router>
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
+        <Route exact path='/' component={MovieList} />
+        <Route path='/movies/:id' component={Movie} />
     </Router>
   );
 }
